fb0ad6ff69af1e910dde27c8475bb71091f4061f
@be <abeahmed2@gmail.com>
2017-10-17 11:14:56 -0400

    Added firebase function to monitor for added replies and calculate sentiment score for the reply. Currently the reply sentiment is calculated and printed out in the console. We want to store this in the database so the frontend can display it.

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const language = require('@google-cloud/language');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const languageClient = new language.LanguageServiceClient();
+
+exports.processReplySentiment = functions.firestore
+  .document('replies/{replyId}')
+  .onCreate((event) => {
+    var reply = event.data.data();
+
+    var replyText = reply.text;
+    console.log(replyText)
+
+    var document = {
+      content: replyText,
+      type: 'PLAIN_TEXT'
+    };
+
+    var sentiment = languageClient.analyzeSentiment({document: document}).then(
+      (results) => {
+        const sentiment = results[0].documentSentiment;
+        console.log(`Text: ${replyText}`);
+        console.log(`Sentiment score: ${sentiment.score}`);
+        console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+      }
+    );
+});
