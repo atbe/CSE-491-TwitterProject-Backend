e22b297dc39cec90d121414330f5fd3568f609ca
@be <abeahmed2@gmail.com>
2017-11-14 10:55:14 -0500

    Create a new firestore client when inserting into database to avoid RPC failures.

--- TwitterWatcher/TwitterWatcher/Database/db.py
+++ TwitterWatcher/TwitterWatcher/Database/db.py
@@ -1,16 +1,18 @@
 from google.cloud import firestore
-from tweepy import models
-from json import dumps
+from tweepy.models import Status
 
 class Database(object):
+		def __init__(self, project_name: str):
+				self._project_name: str = project_name
 
-		def __init__(self):
-				self._db: firestore.Client = firestore.Client(project='twittertweettracker')
 
-		def insert_tweet(self, status):
-				doc_ref: firestore.DocumentReference = self._db.collection('tweets').document(str(status._json['id']))
+		def insert_tweet(self, status: Status):
+				firestore_client = firestore.Client(project=self._project_name)
+				doc_ref: firestore.DocumentReference = firestore_client.collection('tweets').document(str(status._json['id']))
 				doc_ref.set(status._json)
 
-		def insert_reply(self, status):
-				doc_ref: firestore.DocumentReference = self._db.collection('replies').document(str(status._json['id']))
+
+		def insert_reply(self, status: Status):
+				firestore_client = firestore.Client(project=self._project_name)
+				doc_ref: firestore.DocumentReference = firestore_client.collection('replies').document(str(status._json['id']))
 				doc_ref.set(status._json)
--- TwitterWatcher/TwitterWatcher/UserTracker/user.py
+++ TwitterWatcher/TwitterWatcher/UserTracker/user.py
@@ -1,51 +1,43 @@
 from tweepy import StreamListener, models, API
 from TwitterWatcher.Database import Database
-import logging
-import sys
-
-logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-consoleLogger = logging.StreamHandler(sys.stdout)
-consoleLogger.setFormatter(logFormatter)
-logger.addHandler(consoleLogger)
+from TwitterWatcher.logger import Logger
 
 class UserStreamListener(StreamListener):
 		def __init__(self, api: API, username: str):
 				super().__init__(api=api)
-				self._db = Database()
+				self._db = Database(project_name='twittertweettracker')
 				self._username = username
 
 
 		def on_error(self, status_code):
-				logger.critical(f"\nAPI ERROR: {status_code}")
+				Logger.critical(f"\nAPI ERROR: {status_code}")
 
 
 		def on_status(self, status):
 				if status._json.get('in_reply_to_status_id_str', None):
 						try:
-								logger.info(f"Inserting reply {status.id}")
+								Logger.info(f"Inserting reply {status.id}")
 								self._handle_reply(status)
 						except Exception as e:
-								logger.error(f"Could not insert reply with id {status.id_str}")
-								logger.exception(e)
+								Logger.error(f"Could not insert reply with id {status.id_str}")
+								Logger.exception(e)
 								return True
 				else:
 						try:
 								if status.user.screen_name != self._username:
 										return True
 								self._handle_status(status)
-								logger.info(f"Inserting tweet {status.id}")
+								Logger.info(f"Inserting tweet {status.id}")
 								self._handle_reply(status)
 						except Exception as e:
-								logger.error(f"Could not insert reply with id {status.id_str}")
-								logger.exception(e)
+								Logger.error(f"Could not insert reply with id {status.id_str}")
+								Logger.exception(e)
 								return True
 
 
 		def on_delete(self, status_id, user_id):
 				# TODO: Actually delete the reply/status and respective data
-				logger.warning(f"Delete status {status_id} from user {user_id}")
+				Logger.warning(f"Delete status {status_id} from user {user_id}")
 
 
 		def _handle_reply(self, status) -> None:
