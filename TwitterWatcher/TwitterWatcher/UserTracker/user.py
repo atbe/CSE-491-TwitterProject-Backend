767ae53f0b96861c5ef601f14f577229862e1deb
@be <abeahmed2@gmail.com>
2017-11-14 11:10:01 -0500

    Cleaned up the handle status function and only process replies for tweets we have tracked.

--- TwitterWatcher/TwitterWatcher/UserTracker/user.py
+++ TwitterWatcher/TwitterWatcher/UserTracker/user.py
@@ -1,4 +1,5 @@
-from tweepy import StreamListener, models, API
+from tweepy import StreamListener, API
+from tweepy.models import Status
 from TwitterWatcher.Database import Database
 from TwitterWatcher.logger import Logger
 
@@ -7,6 +8,7 @@ class UserStreamListener(StreamListener):
 				super().__init__(api=api)
 				self._db = Database(project_name='twittertweettracker')
 				self._username = username
+				self._tracked_tweets = set()
 
 
 		def on_error(self, status_code):
@@ -15,24 +17,9 @@ class UserStreamListener(StreamListener):
 
 		def on_status(self, status):
 				if status._json.get('in_reply_to_status_id_str', None):
-						try:
-								Logger.info(f"Inserting reply {status.id}")
-								self._handle_reply(status)
-						except Exception as e:
-								Logger.error(f"Could not insert reply with id {status.id_str}")
-								Logger.exception(e)
-								return True
+						self._handle_reply(status)
 				else:
-						try:
-								if status.user.screen_name != self._username:
-										return True
-								self._handle_status(status)
-								Logger.info(f"Inserting tweet {status.id}")
-								self._handle_reply(status)
-						except Exception as e:
-								Logger.error(f"Could not insert reply with id {status.id_str}")
-								Logger.exception(e)
-								return True
+						return self._handle_status(status)
 
 
 		def on_delete(self, status_id, user_id):
@@ -40,9 +27,26 @@ class UserStreamListener(StreamListener):
 				Logger.warning(f"Delete status {status_id} from user {user_id}")
 
 
-		def _handle_reply(self, status) -> None:
-				Database().insert_reply(status)
+		def _handle_status(self, status: Status):
+				if status.user.screen_name != self._username:
+						return True
+				try:
+						Logger.info(f"Inserting tweet {status.id}")
+						self._db.insert_tweet(status)
+						self._tracked_tweets.add(status.id)
+				except Exception as e:
+						Logger.error(f"Could not insert tweet with id {status.id_str}")
+						Logger.exception(e)
+				return True
 
 
-		def _handle_status(self, status: models.Status):
-				Database().insert_tweet(status)
+		def _handle_reply(self, status: Status) -> bool:
+				if status.in_reply_to_status_id_str not in self._tracked_tweets:
+						return True
+				try:
+						Logger.info(f"Inserting reply {status.id}")
+						self._db.insert_reply(status)
+				except Exception as e:
+						Logger.error(f"Could not insert reply with id {status.id_str}")
+						Logger.exception(e)
+						return True
