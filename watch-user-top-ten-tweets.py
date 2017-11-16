38b2347279568150fb4e40584f2e1f9459119d82
@be <abeahmed2@gmail.com>
2017-11-16 11:09:50 -0500

    Added preprocessing which compresses coordinates to they can be inserted into firebase. Logs get stored to file now.

--- watch-user-top-ten-tweets.py
+++ watch-user-top-ten-tweets.py
@@ -1,9 +1,13 @@
 from TwitterWatcher import Client
 from TwitterWatcher import TwitterUserTracker
+from TwitterWatcher.logger import Logger
+
 
 def main():
 	client: Client = Client()
-	tracker = TwitterUserTracker(input("Username to track: "), client)
+	username = input("Username to track: ").lower().strip()
+	tracker = TwitterUserTracker(username, client)
+	Logger.info(f"Starting collection for user '{username}'")
 	tracker.begin_tracking()
 
 if __name__ == '__main__':
